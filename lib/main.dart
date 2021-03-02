f5f9b70c2e39bc00c4853f9a218cd7b11f1fea92
agarwal-sagar <agarwal.sagar@outlook.com>
2021-03-02 13:09:46 +0530

    76 complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+// import 'package:audioplayers/audioplayers.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -6,9 +8,19 @@ class XylophoneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.blue,
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: FlatButton(
+              onPressed: () {
+                final player = AudioCache();
+                player.play("note1.wav");
+              },
+              child: Text("Click me"),
+            ),
+          ),
         ),
       ),
     );
