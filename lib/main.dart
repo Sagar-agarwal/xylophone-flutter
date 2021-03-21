d12af0afb18bff348567708ffee80b47d288a3ac
agarwal-sagar <agarwal.sagar@outlook.com>
2021-03-21 15:49:30 +0530

    Xylophone app complete

--- lib/main.dart
+++ lib/main.dart
@@ -10,18 +10,51 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.white,
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play("note1.wav");
-              },
-              child: Text("Click me"),
-            ),
-          ),
+          child: XyloScreen(),
         ),
+      ),
+    );
+  }
+}
+
+class XyloScreen extends StatelessWidget {
+  const XyloScreen({
+    Key key,
+  }) : super(key: key);
+
+  void playSound(soundName) {
+    final player = AudioCache();
+    player.play(soundName);
+  }
+
+  Expanded getXyloButton(Color color, String soundName) => Expanded(
+        child: MaterialButton(
+          enableFeedback: false,
+          color: color,
+          child: Text(''),
+          onPressed: () {
+            playSound(soundName);
+          },
+        ),
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          getXyloButton(Colors.red[600], 'note1.wav'),
+          getXyloButton(Colors.orange[600], 'note2.wav'),
+          getXyloButton(Colors.yellow[600], 'note3.wav'),
+          getXyloButton(Colors.green[600], 'note4.wav'),
+          getXyloButton(Colors.teal[600], 'note5.wav'),
+          getXyloButton(Colors.blue[600], 'note6.wav'),
+          getXyloButton(Colors.purple[600], 'note7.wav')
+        ],
       ),
     );
   }
